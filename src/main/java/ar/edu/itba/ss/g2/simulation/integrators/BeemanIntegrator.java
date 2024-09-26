9c8938a43a10ccb89f3b76df8752bfaee53aa5a5
Santiago Ballerini <sballerini@itba.edu.ar>
2024-09-26 20:49:14 -0300

    feat: implement beeman integrator

---
+++ src/main/java/ar/edu/itba/ss/g2/simulation/integrators/BeemanIntegrator.java
@@ -0,0 +1,124 @@
+package ar.edu.itba.ss.g2.simulation.integrators;
+
+import ar.edu.itba.ss.g2.model.Particle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeemanIntegrator implements MovementIntegrator {
+
+    private final List<Particle> particles;
+    private final List<Particle> previousParticles;
+
+    private final double deltaTime;
+
+    private final Equation forceEquation;
+
+    public BeemanIntegrator(List<Particle> particles, Equation forceEquation, double deltaTime) {
+        this.deltaTime = deltaTime;
+        this.particles = new ArrayList<>(particles);
+        this.previousParticles = new ArrayList<>(particles.size());
+
+        // Euler for new pos and vel
+        List<Double> forces = forceEquation.apply(particles);
+        for (int i = 0; i < particles.size(); i++) {
+            Particle particle = particles.get(i);
+            double previousPosition =
+                    particle.getPosition()
+                            - particle.getV() * deltaTime
+                            + (deltaTime * deltaTime / (2 * particle.getMass())) * forces.get(i);
+
+            double previousVelocity =
+                    particle.getV() - (deltaTime / particle.getMass()) * forces.get(i);
+
+            previousParticles.add(
+                    new Particle(
+                            particle.getId(),
+                            previousPosition,
+                            previousVelocity,
+                            particle.getMass()));
+        }
+
+        this.forceEquation = forceEquation;
+    }
+
+    @Override
+    public List<Particle> getState() {
+        return particles.stream().map(Particle::new).toList();
+    }
+
+    @Override
+    public void integrate() {
+
+        List<Double> currentForces = forceEquation.apply(particles);
+        List<Double> previousForces = forceEquation.apply(previousParticles);
+
+        // Positions
+        for (int i = 0; i < particles.size(); i++) {
+
+            Particle particle = particles.get(i);
+            Particle previousParticle = previousParticles.get(i);
+
+            double mass = particle.getMass();
+
+            // r(t)
+            double currentPosition = particle.getPosition();
+
+            // v(t)
+            double currentVelocity = particle.getV();
+
+            // a(t)
+            double currentAcceleration = currentForces.get(i) / mass;
+
+            // a(t-dt)
+            double previousAcceleration = previousForces.get(i) / mass;
+
+            // r(t+dt)
+            double nextPosition =
+                    currentPosition
+                            + currentVelocity * deltaTime
+                            + (2.0 / 3.0) * currentAcceleration * Math.pow(deltaTime, 2)
+                            - (1.0 / 6.0) * previousAcceleration * Math.pow(deltaTime, 2);
+
+            // predicted v(t+dt)
+            double predictedVelocity =
+                    currentVelocity
+                            + (3.0 / 2.0) * currentAcceleration * deltaTime
+                            - (1.0 / 2.0) * previousAcceleration * deltaTime;
+
+            previousParticle.setPosition(currentPosition);
+            previousParticle.setV(currentVelocity);
+
+            particle.setPosition(nextPosition);
+            particle.setV(predictedVelocity);
+        }
+
+        List<Double> nextForces = forceEquation.apply(particles);
+
+        // Correct velocities
+        for (int i = 0; i < particles.size(); i++) {
+
+            Particle particle = particles.get(i);
+            Particle previousParticle = previousParticles.get(i);
+
+            double mass = particle.getMass();
+
+            // v(t)
+            double currentVelocity = previousParticle.getV();
+
+            // a(t-dt), a(t), a(t+dt)
+            double previousAcceleration = previousForces.get(i) / mass;
+            double currentAcceleration = currentForces.get(i) / mass;
+            double nextAcceleration = nextForces.get(i) / mass;
+
+            // corrected v(t+dt)
+            double correctedVelocity =
+                    currentVelocity
+                            + (1.0 / 3.0) * nextAcceleration * deltaTime
+                            + (5.0 / 6.0) * currentAcceleration * deltaTime
+                            - (1.0 / 6.0) * previousAcceleration * deltaTime;
+
+            particle.setV(correctedVelocity);
+        }
+    }
+}
