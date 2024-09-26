e65ba0d001bf5de39bbd4718916aaa6f29414541
Santiago Ballerini <sballerini@itba.edu.ar>
2024-09-26 20:20:45 -0300

    feat: add integrator interfaces and simulation loop

---
+++ src/main/java/ar/edu/itba/ss/g2/simulation/Simulation.java
@@ -0,0 +1,38 @@
+package ar.edu.itba.ss.g2.simulation;
+
+import ar.edu.itba.ss.g2.model.Particle;
+import ar.edu.itba.ss.g2.simulation.integrators.MovementIntegrator;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Simulation {
+
+    private final double timeStep;
+    private final double snapshotStep;
+
+    private final MovementIntegrator integrator;
+
+    private final List<List<Particle>> snapshots;
+
+    public Simulation(double timeStep, double snapshotStep, MovementIntegrator integrator) {
+        this.timeStep = timeStep;
+        this.snapshotStep = snapshotStep;
+        this.integrator = integrator;
+        this.snapshots = new LinkedList<>();
+    }
+
+    public void run(double maxTime) {
+        for (double t = 0, elapsed = 0; t < maxTime; t += timeStep, elapsed += timeStep) {
+            integrator.integrate();
+
+            if (elapsed >= snapshotStep) {
+                snapshots.add(integrator.getState());
+            }
+        }
+    }
+
+    public List<List<Particle>> getSnapshots() {
+        return snapshots;
+    }
+}
---
+++ src/main/java/ar/edu/itba/ss/g2/simulation/integrators/Equation.java
@@ -0,0 +1,10 @@
+package ar.edu.itba.ss.g2.simulation.integrators;
+
+import java.util.List;
+
+import ar.edu.itba.ss.g2.model.Particle;
+
+@FunctionalInterface
+public interface Equation {
+    List<Double> apply(List<Particle> state);
+}
