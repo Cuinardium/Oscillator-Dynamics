00f832d27d53903f155d634c7b45a925d41a90d9
Gonzalo A. Martone <gmartone@itba.edu.ar>
2024-09-27 12:11:30 -0300

    feat: add parser for ej2

---
+++ src/main/java/ar/edu/itba/ss/g2/dampened/config2/ArgParser.java
@@ -0,0 +1,174 @@
+package ar.edu.itba.ss.g2.dampened.config2;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ArgParser {
+
+    private static final List<Option> OPTIONS =
+            List.of(
+                    new Option("m", "mass", true, "Mass (kg)"),
+                    new Option("k", "spring", true, "Spring constant (N/m)"),
+                    new Option("A", "aplitude", true, "Force applied amplitude (m)"),
+                    new Option("l0", "l0", true, "Distance between particles (m)"),
+                    new Option("N", "N", true, "Numero de particulas"),
+
+                    new Option("tf", "time", true, "Final time (s)"),
+                    new Option("dt", "delta", true, "Integration time step (s)"),
+                    new Option("dt2", "delta2", true, "Snapshot time step (s)"),
+                    new Option(
+                            "i",
+                            "integrator",
+                            true,
+                            "Movement integration scheme (beeman | verlet | gear)"),
+                    new Option("out", "output", true, "Output directory"),
+                    new Option("h", "help", false, "Print help"));
+
+    private final String[] args;
+    private final Options options;
+
+    public ArgParser(String[] args) {
+        this.args = args;
+
+        this.options = new Options();
+        OPTIONS.forEach(options::addOption);
+    }
+
+    public Configuration parse() {
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd;
+
+        Options cliOptions = new Options();
+        OPTIONS.forEach(cliOptions::addOption);
+
+        try {
+            // Parse the arguments
+            cmd = parser.parse(cliOptions, args);
+
+            // Use the builder pattern for creating Configuration instance
+            Configuration.Builder builder = new Configuration.Builder();
+            if (cmd.hasOption("h")) {
+                printHelp();
+                return null;
+            }
+            if (!cmd.hasOption("m")
+                    || !cmd.hasOption("k")
+                    || !cmd.hasOption("A")
+                    || !cmd.hasOption("l0")
+                    || !cmd.hasOption("N")
+                    || !cmd.hasOption("tf")
+                    || !cmd.hasOption("dt")
+                    || !cmd.hasOption("dt2")
+                    || !cmd.hasOption("i")
+                    || !cmd.hasOption("out")) {
+                System.out.println("Error: Missing parameters. All parameters are required.");
+                return null;
+            }
+
+            // Parse and set parameters, with individual error messages for each
+            try {
+                builder.setM(Double.parseDouble(cmd.getOptionValue("m")));
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Invalid format for mass (m). Expected a valid number.");
+                return null;
+            }
+
+            try {
+                builder.setK(Double.parseDouble(cmd.getOptionValue("k")));
+            } catch (NumberFormatException e) {
+                System.out.println(
+                        "Error: Invalid format for spring constant (k). Expected a valid number.");
+                return null;
+            }
+
+            try {
+                builder.setA(Double.parseDouble(cmd.getOptionValue("A")));
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Invalid format for Amplitude (A). Expected a valid number.");
+                return null;
+            }
+
+            try {
+                builder.setL0(Double.parseDouble(cmd.getOptionValue("l0")));
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Invalid format for Distance between particles (l0). Expected a valid number.");
+                return null;
+            }
+
+            try {
+                builder.setN(Double.parseDouble(cmd.getOptionValue("N")));
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Invalid format for number of particles (N). Expected a valid number.");
+                return null;
+            }
+
+            try {
+                builder.setTf(Double.parseDouble(cmd.getOptionValue("tf")));
+            } catch (NumberFormatException e) {
+                System.out.println(
+                        "Error: Invalid format for final time (tf). Expected a valid number.");
+                return null;
+            }
+
+            try {
+                builder.setDt(Double.parseDouble(cmd.getOptionValue("dt")));
+            } catch (NumberFormatException e) {
+                System.out.println(
+                        "Error: Invalid format for integration time step (dt). Expected a valid"
+                                + " number.");
+                return null;
+            }
+
+            try {
+                builder.setDt2(Double.parseDouble(cmd.getOptionValue("dt2")));
+            } catch (NumberFormatException e) {
+                System.out.println(
+                        "Error: Invalid format for snapshot time step (dt2). Expected a valid"
+                                + " number.");
+                return null;
+            }
+
+            String integrator = cmd.getOptionValue("i").strip();
+            List<String> integrators = List.of("beeman", "verlet", "gear");
+            if (!integrators.contains(integrator)) {
+                System.out.println(
+                        "Error: Invalid integrator. Expected one of: "
+                                + integrators.stream().reduce((a, b) -> a + ", " + b).orElse(""));
+                return null;
+            }
+
+            builder.setIntegrator(integrator);
+
+            builder.setOutputDir(cmd.getOptionValue("out"));
+
+            // Build and return the configuration object
+            return builder.build();
+
+        } catch (Exception e) {
+            System.out.println("Error: Missing or invalid parameters. " + e.getMessage());
+        }
+
+        return null; // Return null if there was an error
+    }
+
+    public void printHelp() {
+
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.setOptionComparator(Comparator.comparingInt(OPTIONS::indexOf));
+
+        formatter.setLeftPadding(4);
+        formatter.setWidth(120);
+
+        String commandLineSyntax =
+                "java -jar dampened-oscillator-jar-with-dependencies.jar" + " [options]";
+
+        formatter.printHelp(commandLineSyntax, options);
+    }
+}
---
+++ src/main/java/ar/edu/itba/ss/g2/dampened/config2/Configuration.java
@@ -0,0 +1,140 @@
+package ar.edu.itba.ss.g2.dampened.config2;
+
+public class Configuration {
+    private final double m;
+    private final double k;
+    private final double A;
+    private final double l0;
+    private final double N;
+    private final double tf;
+    private final double dt;
+    private final double dt2;
+
+    private final String integrator;
+
+    private final String outputDir;
+
+    private Configuration(Builder builder) {
+        this.m = builder.m;
+        this.k = builder.k;
+        this.A = builder.A;
+        this.l0 = builder.l0;
+        this.N = builder.N;
+        this.tf = builder.tf;
+        this.dt = builder.dt;
+        this.dt2 = builder.dt2;
+
+        this.integrator = builder.integrator;
+        
+        this.outputDir = builder.outputDir;
+    }
+
+    public double getM() {
+        return m;
+    }
+
+    public double getK() {
+        return k;
+    }
+
+    public double getA() {
+        return A;
+    }
+
+    public double getL0() {
+        return l0;
+    }
+
+    public double getN() {
+        return N;
+    }
+
+    public double getTf() {
+        return tf;
+    }
+
+    public double getDt() {
+        return dt;
+    }
+
+    public double getDt2() {
+        return dt2;
+    }
+
+    public String getIntegrator() {
+        return integrator;
+    }
+
+    public String getOutputDir() {
+        return outputDir;
+    }
+
+    public static class Builder {
+        private double m;
+        private double k;
+        private double A;
+        private double l0;
+        private double N;
+        private double tf;
+        private double dt;
+        private double dt2;
+
+        private String integrator;
+
+        private String outputDir;
+
+        public Builder setM(double m) {
+            this.m = m;
+            return this;
+        }
+
+        public Builder setK(double k) {
+            this.k = k;
+            return this;
+        }
+
+        public Builder setA(double A) {
+            this.A = A;
+            return this;
+        }
+
+        public Builder setL0(double l0) {
+            this.l0 = l0;
+            return this;
+        }
+
+        public Builder setN(double N) {
+            this.N = N;
+            return this;
+        }
+
+        public Builder setTf(double tf) {
+            this.tf = tf;
+            return this;
+        }
+
+        public Builder setDt(double dt) {
+            this.dt = dt;
+            return this;
+        }
+
+        public Builder setDt2(double dt2) {
+            this.dt2 = dt2;
+            return this;
+        }
+
+        public Builder setIntegrator(String integrator) {
+            this.integrator = integrator;
+            return this;
+        }
+
+        public Builder setOutputDir(String outputDir) {
+            this.outputDir = outputDir;
+            return this;
+        }
+
+        public Configuration build() {
+            return new Configuration(this);
+        }
+    }
+}
